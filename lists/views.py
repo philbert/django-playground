fe6221f4af2c14e00e94dab6d6280aa1523f408c
Philip Cheong <isctsf@gmail.com>
2016-01-17 17:30:29 +0100

    simplify view by using form's save method

--- lists/views.py
+++ lists/views.py
@@ -12,7 +12,7 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, 'form': form})
 
@@ -20,12 +20,8 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
 
-#def add_item(request, list_id):
-#    list_ = List.objects.get(id=list_id)
-#    Item.objects.create(text=request.POST['text'], list=list_)
-#    return redirect('/lists/%d/' % (list_.id,))
