9705de6fa7bb0555dff6a491db196683d5e2045c
Philip Cheong <isctsf@gmail.com>
2016-03-06 23:27:23 +0100

    add first tests for blog and correct (deliberate?) spelling mistake

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
+"""
+testcases for blog
+"""
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Entry
+
+
+class EntryModelTest(TestCase):
+    """ obey the testing goat """
+    def test_string_represenation(self):
+        """ pylint really loves documentation """
+        entry = Entry(title="My entry title")
+        self.assertEqual(str(entry), entry.title)
+
+    def test_verbose_name_plural(self):
+        """ is this a setup? """
+        self.assertEqual(str(Entry._meta.verbose_name_plural), "entries")
