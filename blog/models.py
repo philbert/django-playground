c313de24d68339b984e99737890fb9f4894f021f
Philip Cheong <isctsf@gmail.com>
2016-03-06 17:50:02 +0100

    add new blog app

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+"""
+blog models docstring
+"""
+from django.db import models
+
+class Entry(models.Model):
+    """ Entry class """
+    title = models.CharField(max_length=500)
+    author = models.ForeignKey('auth.User')
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
