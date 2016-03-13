b8bc1290525b5434ed4c924e8863f76e8f040d40
Philip Cheong <isctsf@gmail.com>
2016-03-13 22:03:45 +0100

    create base.html template and inherit to blog.html. Create tests for blog entries

--- blog/tests.py
+++ blog/tests.py
@@ -2,6 +2,7 @@
 testcases for blog
 """
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 from .models import Entry
 
@@ -24,3 +25,25 @@ class ProjectTests(TestCase):
         """ deviating from the example had to happen """
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
+
+class HomePageTests(TestCase):
+    """ test whether our blog entries show up on the homepage """
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='some_user')
+
+    def test_one_entry(self):
+        """ test one """
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        response = self.client.get('/blog/')
+        self.assertContains(response, '1-title')
+        self.assertContains(response, '1-body')
+
+    def test_two_entries(self):
+        """ test two """
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        Entry.objects.create(title='2-title', body='2-body', author=self.user)
+        response = self.client.get('/blog/')
+        self.assertContains(response, '1-title')
+        self.assertContains(response, '1-body')
+        self.assertContains(response, '2-title')
+
